391c92f8ed064af8df4302742b48ba0366553018
euviniciospinheiro <vinicios.pinheiro2018@gmail.com>
2023-06-14 19:55:41 -0300

    escolhas de cores

--- src/App.java
+++ src/App.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         liga4();
+        Scanner teclado = new Scanner(System.in);
+
+        char cores;
+        
+        do {
+            System.out.println("Escolha uma cor V ou A: ");
+            cores = teclado.next().charAt(0);
+            // Convertendo o caracter para maiúsculo
+            cores = Character.toUpperCase(cores);
+            System.out.println(cores);
+        } while (cores != 'V' && cores != 'A');
     }
     public static void liga4() {
         Scanner sc = new Scanner(System.in);
