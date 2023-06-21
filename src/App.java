b69f20b7df2a368f858d845b41af4b25eeaa8b72
Vmp099 <viniciusmendespavesi@gmail.com>
2023-06-21 19:49:22 -0300

    adicionaJogo (now working)

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
@@ -72,26 +73,47 @@ public class App {
 
     public void adicionaTabela(char[][]tabuleiro, char corPc, char corUser,int posicaoEscolhida, Scanner sc){
         int posicao = posicaoEscolhida;
+        
+        // jogador
         do {
             mostraJogo(tabuleiro);
             System.out.println("Informe a coluna desejada: (0 a 6)");
             posicao = sc.nextInt();
-        } while (posicao <= 0 && posicao >= 6);
-        
-        if(tabuleiro[posicao][0] == 'B'){
-            for (int i = 5; i >= 0; i--) {
-                if(tabuleiro[posicao][i] == 'B'){
-                    tabuleiro[posicao][i] = corUser;
-                    break;
-                } 
+
+            if(tabuleiro[posicao][0] == 'B'){
+                for (int i = 5; i >= 0; i--) {
+                    if(tabuleiro[posicao][i] == 'B'){
+                        tabuleiro[posicao][i] = corUser;
+                        break;
+                    }   
+                }
+            }else{
+                posicao = 7;
+                System.out.println("teste!");
+                System.out.println("Coluna Cheia!");
             }
-        }else{
-            
-        }
+        } while (posicao < 0 || posicao > 6);
+
+        //pc
+
+        do {
+            Random gerador = new Random();
+            posicao = (int) gerador.nextInt(7);
+            if(tabuleiro[posicao][0] == 'B'){
+                for (int i = 5; i >= 0; i--) {
+                    if(tabuleiro[posicao][i] == 'B'){
+                        tabuleiro[posicao][i] = corPc;
+                        break;
+                    }   
+                }
+            }else{
+                posicao = 7;
+            }
+        } while (posicao < 0 || posicao > 6);
         
+        
+
         mostraJogo(tabuleiro);
     }
     
 }
-
-
