66708e43fb0751445a2f5f03a2cfaf139979781f
euviniciospinheiro <vinicios.pinheiro2018@gmail.com>
2023-06-27 17:26:03 -0300

    dddddddddd

--- src/App.java
+++ src/App.java
@@ -1,12 +1,29 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        new App();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean jogarNovamente = false;
+
+        do {
+            jogarNovamente = false;
+            jogar();
+            
+            System.out.println("Deseja jogar novamente? (S/N)");
+            char resposta = sc.next().charAt(0);
+            resposta = Character.toUpperCase(resposta);
+
+            if (resposta == 'S') {
+                jogarNovamente = true;
+            }
+        } while (jogarNovamente);
+
+        System.out.println("Obrigado por jogar!");
+        sc.close();
     }
 
-    private App() {
-        Scanner sc = new Scanner(System.in);
+    public static void jogar() {
         final int linha = 6, coluna = 7;
         char[][] tabuleiro = new char[coluna][linha];
         char escolhaCor = 'B';
@@ -16,12 +33,12 @@ public class App {
                 tabuleiro[c][l] = 'B';
             }
         }
-       char corUser = escolhaUsuario(escolhaCor, sc);
-       char corPc   = escolhaPc(corUser);
-       System.out.println("Cor Usuário: " + corUser + "\nCor Pc: " + corPc);
-        adicionaTabela(tabuleiro, corPc, corUser, posicaoEscolhida, sc);
+        Scanner sc = new Scanner(System.in);
+        char corUser = escolhaUsuario(escolhaCor, sc);
+        char corPc   = escolhaPc(corUser);
+        System.out.println("Cor Usuário: " + corUser + "\nCor Pc: " + corPc);
 
-         while (true) {
+        while (true) {
             adicionaTabela(tabuleiro, corPc, corUser, posicaoEscolhida, sc);
             if (verificaVitoria(tabuleiro, corUser)) {
                 System.out.println("Parabéns! Você venceu!");
@@ -36,11 +53,9 @@ public class App {
                 break;
             }
         }
-
-        sc.close();
     }
 
-    public void mostraJogo(char[][] tabuleiro) {
+    public static void mostraJogo(char[][] tabuleiro) {
         System.out.println("=============================");
         System.out.println("| 0 | 1 | 2 | 3 | 4 | 5 | 6 |");
         System.out.println("=============================");
@@ -53,7 +68,7 @@ public class App {
         System.out.println("=============================");
     }
 
-    public char escolhaUsuario(char escolhaCor, Scanner sc) {
+    public static char escolhaUsuario(char escolhaCor, Scanner sc) {
         char escolhaUser = escolhaCor;
         do {
             System.out.println("Escolha uma cor V ou A: ");
@@ -63,74 +78,62 @@ public class App {
         } while (escolhaUser != 'V' && escolhaUser != 'A');
         return escolhaUser;
     }
-    public char escolhaPc(char escolhaUser){
+
+    public static char escolhaPc(char escolhaUser) {
         char corPc = 'B';
         if (escolhaUser == 'A') {
-        corPc = 'V';
-       }else if (escolhaUser == 'V') {
-        corPc = 'A';
-       }else{
-        corPc = 'B';
-       }
-       return corPc;
+            corPc = 'V';
+        } else if (escolhaUser == 'V') {
+            corPc = 'A';
+        } else {
+            corPc = 'B';
+        }
+        return corPc;
     }
 
-    //Perguntar se o Jogador que jogar novamente
-    public void usuarioJogarNovamente(char resposta, Scanner sc) {   
-        do {
-            System.out.println("Deseja jogar novamente? (S/N)");
-            resposta = sc.next().charAt(0);
-            resposta = Character.toUpperCase(resposta);
-        } while (resposta == 'S');
-        System.out.println("Obrigado por jogar!");
-    }
-
-    public void adicionaTabela(char[][]tabuleiro, char corPc, char corUser,int posicaoEscolhida, Scanner sc){
+    public static void adicionaTabela(char[][] tabuleiro, char corPc, char corUser, int posicaoEscolhida, Scanner sc) {
         int posicao = posicaoEscolhida;
-        
+
         // jogador
         do {
             mostraJogo(tabuleiro);
             System.out.println("Informe a coluna desejada: (0 a 6)");
             posicao = sc.nextInt();
 
-            if(tabuleiro[posicao][0] == 'B'){
+            if (tabuleiro[posicao][0] == 'B') {
                 for (int i = 5; i >= 0; i--) {
-                    if(tabuleiro[posicao][i] == 'B'){
+                    if (tabuleiro[posicao][i] == 'B') {
                         tabuleiro[posicao][i] = corUser;
                         break;
-                    }   
+                    }
                 }
-            }else{
+            } else {
                 posicao = 7;
                 System.out.println("teste!");
                 System.out.println("Coluna Cheia!");
             }
         } while (posicao < 0 || posicao > 6);
 
-        //pc
-
+        // pc
         do {
             Random gerador = new Random();
-            posicao = (int) gerador.nextInt(7);
-            if(tabuleiro[posicao][0] == 'B'){
+            posicao = gerador.nextInt(7);
+            if (tabuleiro[posicao][0] == 'B') {
                 for (int i = 5; i >= 0; i--) {
-                    if(tabuleiro[posicao][i] == 'B'){
+                    if (tabuleiro[posicao][i] == 'B') {
                         tabuleiro[posicao][i] = corPc;
                         break;
-                    }   
+                    }
                 }
-            }else{
+            } else {
                 posicao = 7;
             }
         } while (posicao < 0 || posicao > 6);
-        
-        
 
         mostraJogo(tabuleiro);
     }
-           public boolean verificaVitoria(char[][] tabuleiro, char cor) {
-        
+
+    public static boolean verificaVitoria(char[][] tabuleiro, char cor) {
         // Verifica vitória na horizontal
         for (int l = 0; l < 6; l++) {
             for (int c = 0; c < 4; c++) {
@@ -174,7 +177,7 @@ public class App {
         return false;
     }
 
-    public boolean tabuleiroCheio(char[][] tabuleiro) {
+    public static boolean tabuleiroCheio(char[][] tabuleiro) {
         for (int c = 0; c < 7; c++) {
             if (tabuleiro[c][0] == 'B') {
                 return false;
@@ -183,4 +186,3 @@ public class App {
         return true;
     }
 }
-        
